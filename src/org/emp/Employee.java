176ce4147f86ce898e27ccbb93cd2e6f3c44fc85
partha16june <parthamechsarathi@gmail.com>
2022-01-16 19:58:58 +0530

    hellooo

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,41 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		// TODO Auto-generated method stub
+System.out.println("483");
+
+	}
+	private void empName() {
+		// TODO Auto-generated method stub
+System.out.println("partha");
+
+	}
+	private void empDob() {
+		// TODO Auto-generated method stub
+System.out.println("16/06/1992");
+
+	}
+private void empPhone() {
+	// TODO Auto-generated method stub
+System.out.println("123456789");
+
+}
+private void empEmail() {
+	// TODO Auto-generated method stub
+System.out.println("greens@gmail.com");
+
+}private void empAdress() {
+	// TODO Auto-generated method stub
+System.out.println("chennai");
+
+}public static void main(String[] args) {
+	Employee a=new Employee();
+	a.empId();
+	a.empName();
+	a.empDob();
+	a.empPhone();
+	a.empEmail();
+	a.empAdress();
+}
+}
